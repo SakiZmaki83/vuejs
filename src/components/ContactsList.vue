1400e0874c09858d1c7450f7c6093132a9d1acfa
jelena <jelena>
2018-04-23 20:59:58 +0200

    first

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <table>
+    
+    <thead>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Email</th>
+    </thead>
+    <tbody>
+    <tr v-for="(contact, key) in contacts" :key="key">
+        <td>{{ contact.firstName}}</td>
+        <td>{{ contact.lastName}}</td>
+          <td>{{ contact.email}}</td>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+    </template>
+ 
+
+<script>
+export default {
+  name: 'ContactsList',
+  data() {
+    return {
+      contacts: [
+        {firstName: 'John', lastName: 'Doe', email: 'john@doe.com'},
+        {firstName: 'Mike', lastName: 'Dane', email: 'mike@dane.com'},
+        {firstName: 'Steve', lastName: 'Kolin', email: 'steve@kolin.com'}
+      ]
+    }
+  }
+}
+</script>
+
